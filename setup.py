f26f9db2fe1697627db4ea85f17fde3fed74f634
Peter Vegh <pvegh@posteo.uk>
2020-07-24 18:07:32 +0100

    Specify license

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('tatapov/version.py').read()) # loads __version__
+exec(open("tatapov/version.py").read())  # loads __version__
 
 setup(
-    name='tatapov',
+    name="tatapov",
     version=__version__,
-    author='Zulko',
-    url='https://github.com/Edinburgh-Genome-Foundry/tatapov',
-    description='Provides DNA overhang misannealing data (Potatov 2018).',
-    long_description=open('README.rst').read(),
-    license='see LICENSE.txt',
+    author="Zulko",
+    url="https://github.com/Edinburgh-Genome-Foundry/tatapov",
+    description="Provides DNA overhang misannealing data (Potatov 2018).",
+    long_description=open("README.rst").read(),
+    license="MIT",
     keywords="overhang golden gate dna annealing mismatch potapov",
-    packages=find_packages(exclude='docs'),
+    packages=find_packages(exclude="docs"),
     include_package_data=True,
-    install_requires=["pandas", "appdirs", "flametree", "xlrd"])
+    install_requires=["pandas", "appdirs", "flametree", "xlrd"],
+)
