b91ae4805a27b4ac0d9b7766bb05242e1d6e7e6d
Peter Vegh <pvegh@posteo.uk>
2021-01-20 16:11:04 +0000

    Format to Black

--- tatapov/__init__.py
+++ tatapov/__init__.py
@@ -3,6 +3,12 @@
 # __all__ = []
 
 from .annealing_data import annealing_data
-from .utils import (data_subset, plot_data, relative_self_annealings,
-                    cross_annealings, reverse_complement, list_overhangs)
+from .utils import (
+    data_subset,
+    plot_data,
+    relative_self_annealings,
+    cross_annealings,
+    reverse_complement,
+    list_overhangs,
+)
 from .version import __version__
